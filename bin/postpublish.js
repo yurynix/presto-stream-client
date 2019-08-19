1ef9d05283971b62891f62bcb13c920d4665d211
Jonathan David Ritchie <jonathandavid.ritchie@grabtaxi.com>
2019-08-19 11:23:30 +0700

    fix postpublish

--- bin/postpublish.js
+++ bin/postpublish.js
@@ -1,5 +1,4 @@
 const exec = require('child_process').exec;
 const path = require('path');
 exec('git checkout -- index.js',{cwd:path.resolve(__dirname,'..','lib')},function(){
-    process.close(0);
 });
