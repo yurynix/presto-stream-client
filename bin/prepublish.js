96d5b9d6dfc9aeec803e9655e2562eb1247b14da
Jonathan David Ritchie <jonathandavid.ritchie@grabtaxi.com>
2019-08-19 11:22:20 +0700

    comma & bracket in wrong place

--- bin/prepublish.js
+++ bin/prepublish.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 const path = require('path');
 const { version } = require(path.resolve(__dirname,'../package.json'));
 const index = fs.readFileSync(path.resolve(__dirname,'../lib/index.js'),'utf8');
-fs.writeFileSync(path.resolve(__dirname,'../lib/index.js'),index.replace(`require('../package.json').version`),`"${version}"`);
+fs.writeFileSync(path.resolve(__dirname,'../lib/index.js'),index.replace(`require('../package.json').version`,`"${version}"`));
